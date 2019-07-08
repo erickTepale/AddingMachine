903e6840317b6275b2ce4bbe17ceee1fdf46c585
Erick Tepale <e.tepale@gmail.com>
2019-07-07 21:50:46 -0400

    finished backend just need to display on website

--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Calc from "../calculator/calc";
 
 @Component({
   selector: 'app-text-input',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-input.component.css']
 })
 export class TextInputComponent implements OnInit {
+  calc:Calc;// Wont work inside onClick because it doesnt know the reference to this.calc <- so i made a fresh one inside the method
 
-  constructor() { }
+  constructor() {
+    this.calc = new Calc();
+  }
 
   ngOnInit() {
+    document.getElementById("blurr").addEventListener("click", this.onClick);
+  }
+
+  onClick(){
+    let superCalc:Calc = new Calc();
+    let output = document.getElementById("output") as InnerHTML;
+    let input:string = (<HTMLInputElement>document.getElementById("gUnit")).value;
+    
+
+    output.innerHTML += ("> " + input);
+    output.innerHTML += (">> " + superCalc.run(input));
+
   }
 
 }
